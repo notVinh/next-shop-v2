14815b88159ac6db4ea2d573e4472ce0bfb845e7
notVinh <dqv4work@gmail.com>
2025-01-06 18:07:04 +0700

    update home 1

--- components/NewItemCard.tsx
+++ components/NewItemCard.tsx
@@ -4,13 +4,15 @@ import React from "react";
 const NewItemCard = () => {
   return (
     <div>
-      <Image
-        className="w-96 h-96 rounded-2xl"
-        src={"/images/new.png"}
-        alt="pro"
-        width={300}
-        height={300}
-      ></Image>
+      <div className="w-96 h-96 overflow-hidden rounded-2xl">
+        <Image
+          src={"/images/new.png"}
+          alt="pro"
+          layout="relative"
+          width={1000}
+          height={1000}
+        ></Image>
+      </div>
       <div className="flex-col flex gap-1 mt-5 w-full items-center md:items-start">
         <div className="font-bold">Loro Piana</div>
         <div className="text-xs text-gray-400">Slim-Fit Strip</div>
--- components/RecommenItemCard.tsx
+++ components/RecommenItemCard.tsx
@@ -5,14 +5,16 @@ import { Button } from "./ui/button";
 const RecommenItemCard = () => {
   return (
     <div className="relative">
-      <Image
-        className="w-96 h-96 rounded-2xl"
-        src={"/images/new.png"}
-        alt="pro"
-        width={300}
-        height={300}
-      ></Image>
-      <div className="flex-col flex gap-1 p-3 absolute bottom-0 left-0">
+      <div className="w-96 h-96 overflow-hidden rounded-2xl">
+        <Image
+          src={"/images/new.png"}
+          alt="pro"
+          layout="relative"
+          width={1000}
+          height={1000}
+        ></Image>
+      </div>
+      <div className="flex-col flex gap-1 p-3 absolute top-0 left-0">
         <div className="font-bold text-3xl">Nike Dunk</div>
         <Button className="rounded-full w-20">Shop now</Button>
       </div>
